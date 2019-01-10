1b4bea41e0e2c4b1d39029c524f386a062202de9
admin <admin@example.com>
2019-01-10 06:36:28 +0530

    Changed the HTML for the site.

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,20 @@
+"""mysite URL Configuration
 
+[...]
+"""
 from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+#from django.contrib import admin
 from django.urls import include,path
-from django.conf.urls import url
-urlpatterns = (
-    url(r'^admin/', admin.site.urls),
-    url(r'^music/', include('music.urls')),
+#from django.conf.urls import url
+#urlpatterns = (
+#    url(r'^admin/', admin.site.urls),
+    #url(r'^music/', include('music.urls')),
     # path(r'music', include('music.urls')),
-)
+#)
