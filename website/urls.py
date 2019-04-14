bc4c45d178e8d343b4ae549b067169986b4eeed3
satyendraniranjan <sat.n760@gmail.com>
2019-04-15 01:48:49 +0530

    change account to accounts

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('account/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new]
     path('tracker/', include('Tracker.urls')),
 
